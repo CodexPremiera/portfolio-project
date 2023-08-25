74aca8ba98524a247b9422146253b3f4b1e073ad
Ashley Ken Comandao <ashleyken.professional@gmail.com>
2023-08-25 13:51:17 +0800

    correct the errors on dropdown mechanisms

--- resources/script/index.js
+++ resources/script/index.js
@@ -4,12 +4,12 @@ let hamburgerClose = document.getElementById("hamburger_menu_close");
 let dropdownMenu = document.getElementById("header_dropdown");
 
 const openDropdown = () => {
-    dropdownMenu.style.display = 'none';
+    dropdownMenu.style.display = 'block';
     hamburgerMenu.style.display = "none"
     hamburgerClose.style.display = "block"
 }
 const closeDropdown = () => {
-    dropdownMenu.style.display = 'block';
+    dropdownMenu.style.display = 'none';
     hamburgerMenu.style.display = "block"
     hamburgerClose.style.display = "none"
 }
