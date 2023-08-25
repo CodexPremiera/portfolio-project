ebbe12b0125de0c5b328ce8878df061745fae118
Ashley Ken Comandao <ashleyken.professional@gmail.com>
2023-08-25 10:27:50 +0800

    add javascript action for dropdown menu

---
+++ resources/script/index.js
@@ -0,0 +1,18 @@
+/* ==================== header dropdown menu ==================== */
+let hamburgerMenu = document.getElementById("hamburger_menu");
+let hamburgerClose = document.getElementById("hamburger_menu_close");
+let dropdownMenu = document.getElementById("header_dropdown");
+
+const openDropdown = () => {
+    dropdownMenu.style.display = 'none';
+    hamburgerMenu.style.display = "none"
+    hamburgerClose.style.display = "block"
+}
+const closeDropdown = () => {
+    dropdownMenu.style.display = 'block';
+    hamburgerMenu.style.display = "block"
+    hamburgerClose.style.display = "none"
+}
+
+hamburgerMenu.onclick = openDropdown;
+hamburgerClose.onclick = closeDropdown;
